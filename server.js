c8f1f2ff80ccac7f23bdc8cfbd15977fd358f72a
Dev533 <35181736+Dev533@users.noreply.github.com>
2018-04-22 00:27:04 -0700

    <message>

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ var express = require('express')
 var bp = require('body-parser')
 var path = require('path')
 
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
+
